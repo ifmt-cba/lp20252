8d49698cac045b6f08ceb3c4d09350c23f0ed134
Preti, João Paulo <jppreti@gmail.com>
2025-08-19 08:18:00 -0400

    transformando as questoes em comandos

--- lista1.py
+++ lista1.py
@@ -2,118 +2,144 @@
 Exercícios sobre os comandos básicos em Python
 '''
 
+def cabecalho(titulo):
+    print('========================================')
+    print(f'============= {titulo} ===============')
+    print('========================================')
+
 #1. Faça um programa que imprima o seu nome.
-print('João Paulo')
+def q1():
+    cabecalho('QUESTÃO 1')
+    print('João Paulo')
 
 #2. Faça um programa que imprima o produto dos valores 30 e 27.
-print(30*27)
+def q2():
+    cabecalho('QUESTÃO 2')
+    print(30*27)
 
 #3. Faça um programa que imprima a média aritmética entre os números 5, 8, 12.
-print(f'(5+8+12)/3 = {(5+8+12)/3}')
+def q3():
+    cabecalho('QUESTÃO 3')
+    print(f'(5+8+12)/3 = {(5+8+12)/3}')
 
 #4. Faça um programa que leia e imprima um número inteiro.
-numero = int(input('Digite um número inteiro: '))
-print(f'O valor digitado foi: {numero}')
+def q4():
+    cabecalho('QUESTÃO 4')
+    numero = int(input('Digite um número inteiro: '))
+    print(f'O valor digitado foi: {numero}')
 
 #5. Faça um programa que leia dois números reais e os imprima.
-num1 = float(input('Digite o primeiro número real: '))
-num2 = float(input('Digite o segundo número real: '))
-print(num1, num2)
+def q5():
+    num1 = float(input('Digite o primeiro número real: '))
+    num2 = float(input('Digite o segundo número real: '))
+    print(num1, num2)
 
 #6. Faça um programa que leia um número inteiro e imprima o seu
 #   antecessor e o seu sucessor.
-num = int(input('Digite um número inteiro: '))
-print(f'Antecessor: {num-1}')
-print(f'Sucessor: {num+1}')
+def q6():
+    num = int(input('Digite um número inteiro: '))
+    print(f'Antecessor: {num-1}')
+    print(f'Sucessor: {num+1}')
 
 #7. Faça um programa que leia o nome o endereço e o telefone de
 #   um cliente e ao final, imprima esses dados.
-nome = input('Nome: ')
-endereco = input('Endereço: ')
-telefone = input('Telefone: ')
-resultado = f'''
-Nome: {nome}
-Endereço: {endereco}
-Telefone: {telefone}
-'''
-print(resultado)
+def q7():
+    nome = input('Nome: ')
+    endereco = input('Endereço: ')
+    telefone = input('Telefone: ')
+    resultado = f'''
+    Nome: {nome}
+    Endereço: {endereco}
+    Telefone: {telefone}
+    '''
+    print(resultado)
 
 #8. Faça um programa que leia dois números inteiros e imprima a
 #   subtração deles.
-num1 = int(input('Número inteiro: '))
-num2 = int(input('Outro número inteiro: '))
-print(num1-num2)
+def q8():
+    num1 = int(input('Número inteiro: '))
+    num2 = int(input('Outro número inteiro: '))
+    print(num1-num2)
 
 #9. Faça um programa que leia um número real e imprima ¼ deste número.
-num = float(input('Número real: '))
-print(num/4)
+def q9():
+    num = float(input('Número real: '))
+    print(num/4)
 
 #10. Faça um programa que leia três números reais e calcule a
 #    média aritmética destes números. Ao final, o programa deve
 #    imprimir o resultado do cálculo.
-a = float(input('Primeiro Número: '))
-b = float(input('Segundo Número: '))
-c = float(input('Terceiro Número: '))
-media = (a+b+c)/3
-print(f'Média: {media}')
+def q10():
+    a = float(input('Primeiro Número: '))
+    b = float(input('Segundo Número: '))
+    c = float(input('Terceiro Número: '))
+    media = (a+b+c)/3
+    print(f'Média: {media}')
 
 #11. Faça um programa que leia dois números reais e calcule as
 #    quatro operações básicas entre estes dois números, adição,
 #    subtração,multiplicação e divisão. Ao final, o programa
 #    deve imprimir os resultados dos cálculos.
-x = float(input('Primeiro número: '))
-y = float(input('Segundo número:'))
-resultado = f'''
-{x} + {y} = {x+y}
-{x} - {y} = {x-y}
-{x} * {y} = {x*y}
-{x} / {y} = {x/y}
-'''
-print(resultado)
+def q11():
+    x = float(input('Primeiro número: '))
+    y = float(input('Segundo número:'))
+    resultado = f'''
+    {x} + {y} = {x+y}
+    {x} - {y} = {x-y}
+    {x} * {y} = {x*y}
+    {x} / {y} = {x/y}
+    '''
+    print(resultado)
 
 #12. Faça um programa que leia um número real e calcule o
 #    quadrado deste número. Ao final, o programa deve
 #    imprimir o resultado do cálculo.
-num = float(input('Número: '))
-print(num*num)
+def q12():
+    num = float(input('Número: '))
+    print(num*num)
 
 #13. Faça um programa que leia o saldo de uma conta poupança e
 #    imprima o novo saldo, considerando um reajuste de 2%.
-saldo = float(input('Saldo: R$ '))
-print(saldo*1.02)
+def q13():
+    saldo = float(input('Saldo: R$ '))
+    print(saldo*1.02)
 
 #14. Faça um programa que leia a base e a altura de um retângulo
 #    e imprima o perímetro (base*2 + altura*2) e a área (base * altura).    
-base = int(input('Base do retângulo: '))
-altura = int(input('Altura do retângulo: '))
-print(f'Perímetro: {base*2+altura*2}')
-print(f'Área: {base*altura}')
+def q14():
+    base = int(input('Base do retângulo: '))
+    altura = int(input('Altura do retângulo: '))
+    print(f'Perímetro: {base*2+altura*2}')
+    print(f'Área: {base*altura}')
 
 #15. Faça um programa que leia o valor de um produto, o percentual
 #    do desconto desejado e imprima o valor do desconto e o valor
 #    do produto subtraindo o desconto.
-valor = float(input('Valor do produto: R$ '))
-perc_desconto = int(input('Percentual do desconto: '))
-desconto = valor * perc_desconto/100
-valor_final = valor - desconto
-print(f'Desconto: R$ {desconto}')
-print(f'Valor final: R$ {valor_final}')
+def q15():
+    valor = float(input('Valor do produto: R$ '))
+    perc_desconto = int(input('Percentual do desconto: '))
+    desconto = valor * perc_desconto/100
+    valor_final = valor - desconto
+    print(f'Desconto: R$ {desconto}')
+    print(f'Valor final: R$ {valor_final}')
 
 #16. Faça um programa que calcule o reajuste do salário de um
 #    funcionário. Para isso, o programa deverá ler o salário atual
 #    do funcionário e ler o percentual de reajuste. Ao final imprimir
 #    o valor do novo salário.
-salario = float(input('Salário: '))
-perc_reajuste = float(input('Percentual de reajuste: '))
-print(f'Novo Salário: R$ {salario*perc_reajuste/100+salario}')
+def q16():
+    salario = float(input('Salário: '))
+    perc_reajuste = float(input('Percentual de reajuste: '))
+    print(f'Novo Salário: R$ {salario*perc_reajuste/100+salario}')
 
 #17. Faça um programa que calcule a conversão entre graus centígrados
 #    e Fahrenheit. Para isso, leia o valor em centígrados e calcule
 #    com base na fórmula a seguir. Após calcular o programa deve
 #    imprimir o resultado da conversão.
 #    F = (9 x C + 160) / 5
-c = float(input('Graus Centígrados: '))
-print(f'Fahrenheit: {(9*c+160)/5}')
+def q17():
+    c = float(input('Graus Centígrados: '))
+    print(f'Fahrenheit: {(9*c+160)/5}')
 
 #18. Faça um programa que calcule a quantidade de litros de combustível
 #    consumidos em uma viagem, sabendo-se que o carro tem autonomia de
@@ -121,7 +147,7 @@ print(f'Fahrenheit: {(9*c+160)/5}')
 #    decorrido na viagem e a velocidade média e aplicar as fórmulas:
 #    D = T x V       L = D / 12
 #    Em que:
-#    • D = Distância percorrida em horas
+#    • D = Distância percorrida
 #    • T = Tempo
 #    • V = Velocidade média
 #    • L = Litros de combustível consumidos
@@ -139,3 +165,4 @@ print(f'Fahrenheit: {(9*c+160)/5}')
 #20. Faça um programa que efetue a apresentação do valor da conversão
 #    em real (R$) de um valor lido em dólar (US$). Para isso, será
 #    necessário também ler o valor da cotação do dólar.
+
