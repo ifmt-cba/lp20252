fec7c017ee0d7ca7cbbc8ffc39ac79443c8f372e
Preti, João Paulo <jppreti@gmail.com>
2025-08-19 08:20:15 -0400

    adicionado cabeçalho em todas as questões

--- lista1.py
+++ lista1.py
@@ -30,6 +30,7 @@ def q4():
 
 #5. Faça um programa que leia dois números reais e os imprima.
 def q5():
+    cabecalho('QUESTÃO 5')
     num1 = float(input('Digite o primeiro número real: '))
     num2 = float(input('Digite o segundo número real: '))
     print(num1, num2)
@@ -37,6 +38,7 @@ def q5():
 #6. Faça um programa que leia um número inteiro e imprima o seu
 #   antecessor e o seu sucessor.
 def q6():
+    cabecalho('QUESTÃO 6')
     num = int(input('Digite um número inteiro: '))
     print(f'Antecessor: {num-1}')
     print(f'Sucessor: {num+1}')
@@ -44,6 +46,7 @@ def q6():
 #7. Faça um programa que leia o nome o endereço e o telefone de
 #   um cliente e ao final, imprima esses dados.
 def q7():
+    cabecalho('QUESTÃO 7')
     nome = input('Nome: ')
     endereco = input('Endereço: ')
     telefone = input('Telefone: ')
@@ -57,12 +60,14 @@ def q7():
 #8. Faça um programa que leia dois números inteiros e imprima a
 #   subtração deles.
 def q8():
+    cabecalho('QUESTÃO 8')
     num1 = int(input('Número inteiro: '))
     num2 = int(input('Outro número inteiro: '))
     print(num1-num2)
 
 #9. Faça um programa que leia um número real e imprima ¼ deste número.
 def q9():
+    cabecalho('QUESTÃO 9')
     num = float(input('Número real: '))
     print(num/4)
 
@@ -70,6 +75,7 @@ def q9():
 #    média aritmética destes números. Ao final, o programa deve
 #    imprimir o resultado do cálculo.
 def q10():
+    cabecalho('QUESTÃO 10')
     a = float(input('Primeiro Número: '))
     b = float(input('Segundo Número: '))
     c = float(input('Terceiro Número: '))
@@ -81,6 +87,7 @@ def q10():
 #    subtração,multiplicação e divisão. Ao final, o programa
 #    deve imprimir os resultados dos cálculos.
 def q11():
+    cabecalho('QUESTÃO 11')
     x = float(input('Primeiro número: '))
     y = float(input('Segundo número:'))
     resultado = f'''
@@ -95,18 +102,21 @@ def q11():
 #    quadrado deste número. Ao final, o programa deve
 #    imprimir o resultado do cálculo.
 def q12():
+    cabecalho('QUESTÃO 12')
     num = float(input('Número: '))
     print(num*num)
 
 #13. Faça um programa que leia o saldo de uma conta poupança e
 #    imprima o novo saldo, considerando um reajuste de 2%.
 def q13():
+    cabecalho('QUESTÃO 13')
     saldo = float(input('Saldo: R$ '))
     print(saldo*1.02)
 
 #14. Faça um programa que leia a base e a altura de um retângulo
 #    e imprima o perímetro (base*2 + altura*2) e a área (base * altura).    
 def q14():
+    cabecalho('QUESTÃO 14')
     base = int(input('Base do retângulo: '))
     altura = int(input('Altura do retângulo: '))
     print(f'Perímetro: {base*2+altura*2}')
@@ -116,6 +126,7 @@ def q14():
 #    do desconto desejado e imprima o valor do desconto e o valor
 #    do produto subtraindo o desconto.
 def q15():
+    cabecalho('QUESTÃO 15')
     valor = float(input('Valor do produto: R$ '))
     perc_desconto = int(input('Percentual do desconto: '))
     desconto = valor * perc_desconto/100
@@ -128,6 +139,7 @@ def q15():
 #    do funcionário e ler o percentual de reajuste. Ao final imprimir
 #    o valor do novo salário.
 def q16():
+    cabecalho('QUESTÃO 16')
     salario = float(input('Salário: '))
     perc_reajuste = float(input('Percentual de reajuste: '))
     print(f'Novo Salário: R$ {salario*perc_reajuste/100+salario}')
@@ -138,6 +150,7 @@ def q16():
 #    imprimir o resultado da conversão.
 #    F = (9 x C + 160) / 5
 def q17():
+    cabecalho('QUESTÃO 17')
     c = float(input('Graus Centígrados: '))
     print(f'Fahrenheit: {(9*c+160)/5}')
 
@@ -153,7 +166,8 @@ def q17():
 #    • L = Litros de combustível consumidos
 #    Ao final, o programa deverá imprimir a distância percorrida e a
 #    quantidade de litros consumidos na viagem.
-
+def q18():
+    cabecalho('QUESTÃO 18')
 
 #19. Faça um programa que calcule o valor de uma prestação em atraso.
 #    Para isso, o programa deve ler o valor da prestação vencida, a
@@ -161,8 +175,11 @@ def q17():
 #    programa deve imprimir o valor da prestação atrasada, o período
 #    de atraso, os juros que serão cobrados pelo período de atraso, o
 #    valor da prestação acrescido dos juros. Considere juros simples.
+def q19():
+    cabecalho('QUESTÃO 19')
 
 #20. Faça um programa que efetue a apresentação do valor da conversão
 #    em real (R$) de um valor lido em dólar (US$). Para isso, será
 #    necessário também ler o valor da cotação do dólar.
-
+def q20():
+    cabecalho('QUESTÃO 20')
